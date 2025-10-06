3800d788e729a773155d48a9ec12082185b2e0af
Philippe GASSMANN <philoops@gmail.com>
2025-10-06 16:22:43 +0200

    move to clap, remove useless envy & serde crate

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,43 @@
-use serde::Deserialize;
+use clap::Parser;
 use slack_hook3::{AttachmentBuilder, PayloadBuilder, Slack};
-use structopt::StructOpt;
 
 /// Send message to slack channels using the Slack Incoming Webhook API
 ///
 /// Note: environment variable are read from environment enriched by .env file found in current directory and parents
-#[derive(StructOpt)]
+#[derive(Parser)]
 struct Opt {
     /// Webhook url, if not present it is read from SWOOK_WEBHOOK_URL environment variable
-    #[structopt(long = "webhook-url", short = "u")]
-    webhook_url: Option<String>,
+    #[arg(long = "webhook-url", short = 'u', env = "SWOOK_WEBHOOK_URL")]
+    webhook_url: String,
     /// Channel to publish to, if not present it is read from SWOOK_CHANNEL environment variable
     ///
     /// If ommited, the message will be published to the default webhook channel
-    #[structopt(long = "channel", short = "c")]
+    #[arg(long = "channel", short = 'c', env = "SWOOK_CHANNEL")]
     channel: Option<String>,
     /// The username (optional)
-    #[structopt(long = "username", short = "n")]
+    #[arg(long = "username", short = 'n')]
     username: Option<String>,
     /// The user icon url (optional)
-    #[structopt(long = "user-icon-url", short = "i")]
+    #[arg(long = "user-icon-url", short = 'i')]
     user_icon_url: Option<String>,
     /// The text of an attachement (optional)
-    #[structopt(long = "attachment")]
+    #[arg(long = "attachment")]
     attachment: Option<String>,
     /// The color of the attachment (valid only if a text has been specified)
-    #[structopt(long = "attachment-color")]
+    #[arg(long = "attachment-color")]
     attachment_color: Option<String>,
     /// The text of the message to publish
     text: String,
 }
 
-#[derive(Deserialize)]
-struct Env {
-    webhook_url: Option<String>,
-    channel: Option<String>,
-}
-
 #[tokio::main]
 async fn main() {
-    let _ = dotenv::dotenv();
-    let env: Option<Env> = envy::prefixed("SWOOK_").from_env().ok();
-    let opt: Opt = Opt::from_args();
-
-    let webhook_url = opt
-        .webhook_url
-        .as_ref()
-        .or(env.as_ref().map(|e| e.webhook_url.as_ref()).flatten())
-        .unwrap_or_else(|| {
-            eprintln!("No webhook url provided!");
-            std::process::exit(1);
-        })
-        .as_str();
-    let channel = opt
-        .channel
-        .as_ref()
-        .or(env.as_ref().map(|e| e.channel.as_ref()).flatten())
-        .map(String::as_str);
+    let _ = dotenvy::dotenv();
+    let opt: Opt = Opt::parse();
 
     let payload = PayloadBuilder::new().text(opt.text);
     // change default webhook channel
-    let payload = match channel {
+    let payload = match &opt.channel {
         Some(channel) => payload.channel(channel),
         None => payload,
     };
@@ -97,7 +74,7 @@ async fn main() {
     .build()
     .unwrap();
     // finally send to slack
-    match Slack::new(webhook_url) {
+    match Slack::new(opt.webhook_url) {
         Ok(slack) => {
             if let Err(e) = slack.send(&payload).await {
                 eprintln!("Unable to notify slack channel: {}", e);
